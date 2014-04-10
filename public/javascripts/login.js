e9b73fed83dfd31fc5050d7949777e005b413090
mlli2 <mlli2@illinois.edu>
2014-04-10 03:50:10 -0500

    Fixed sliding animations on login/register

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -2,5 +2,10 @@ $(".registerButton").click(function(){
     var div = $(this).prev().prev();
     div.slideUp();
     $(this).fadeToggle();
-    $(this).next().fadeToggle();
+    $(this).next().delay(400).slideDown();
+});
+
+$(".signInButton").click(function(e){
+	e.preventDefault();
+	window.location = '/main';
 });
