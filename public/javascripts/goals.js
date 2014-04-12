c3535df019d20e6bc5b1957141442e93f9ae5e9d
EngrishMajor <ken.yht@gmail.com>
2014-04-11 23:50:17 -0500

    added slidable navbar, add goal button, and expanding circles

--- public/javascripts/goals.js
+++ public/javascripts/goals.js
@@ -4,7 +4,7 @@ var UI = UI || {
 	newGoal : true,
 	currGoal : null,
 	dotRadius : 80,
-	dragging : false,
+	dragging : true,
 };
 
 // Click Testing
--- public/javascripts/mainPage.js
+++ public/javascripts/mainPage.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 	$("#main-goal-deadline").datepicker();
+
+	var navToggle = false;
+	$("#main-nav-toggle-container").click(function() {
+		if (navToggle === false) {
+			navToggle = true;
+			$("#main-navbar-container").animate({
+				right: 0
+			}, 200, function() {
+				$("#main-nav-toggle").removeClass("glyphicon-chevron-left");
+				$("#main-nav-toggle").addClass("glyphicon-chevron-right");
+			});
+		} else {
+			navToggle = false;
+			$("#main-navbar-container").animate({
+				right: "-220px"
+			}, 200, function() {
+				$("#main-nav-toggle").removeClass("glyphicon-chevron-right");
+				$("#main-nav-toggle").addClass("glyphicon-chevron-left");
+			});
+		}
+	});
+	
 });
