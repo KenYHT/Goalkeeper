cd81b9bb050ee9966d3f251801e49c7fa8777395
EngrishMajor <ken.yht@gmail.com>
2014-04-24 00:51:04 -0500

    saving goals based off of users also

--- routes/main.js
+++ routes/main.js
@@ -8,12 +8,22 @@ var Goal = require('./../models/goal-model.js').Goal;
 var validator = require('validator');
 
 // Handles POST requests to create/update goals
+/**
+* POST request for saving/updating a goal. Sanitizes the form input, then 
+*/
 exports.saveGoal = function (req, res) {
-	var goalName = validator.escape(req.body.name);
+	var goalName = validator.escape(req.body.title);
 	var goalDescription = validator.escape(req.body.description);
 	var goalDeadline = validator.escape(req.body.deadline);
-	Goal.findOneAndUpdate({ title: goalName },
-		{ user : req.session.user, description: goalDescription, deadline: goalDeadline },
+	var goalTags = req.body.tags;
+
+	if (goalTags) {
+		for (var i = 0; i < goalTags.length; i++)
+			goalTags[i] = validator.escape(goalTags[i]);
+	}
+
+	Goal.findOneAndUpdate({ user: req.session.user, title: goalName },
+		{ title: goalName, user : req.session.user, description: goalDescription, deadline: goalDeadline, tags: goalTags },
 		{ upsert: true },
 		function (err, goal) {
 			if (err){
