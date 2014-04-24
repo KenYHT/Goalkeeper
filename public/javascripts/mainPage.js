85420660a9abaa280edb72ff1f44f3c055ef6b0b
mlli2 <mlli2@illinois.edu>
2014-04-24 02:08:58 -0500

    Added custom wallpapers

--- public/javascripts/mainPage.js
+++ public/javascripts/mainPage.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	});
 });
 
-/*$(switchBackground);
+$(switchBackground);
 var oFReader = new FileReader(),
     rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
 
@@ -44,4 +44,4 @@ function loadImageFile(testEl) {
   var oFile = testEl.files[0];
   if (!rFilter.test(oFile.type)) { alert("You must select a valid image file!"); return; }
   oFReader.readAsDataURL(oFile);
-}*/
+}
